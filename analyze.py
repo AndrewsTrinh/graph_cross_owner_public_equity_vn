7fb175d6ac190f2a5af78e6282bd19f8ddc8f80d
Andrews <andrews.trinh@gmail.com>
2021-09-12 02:59:10 +1000

    remove 1 node relationship

--- analyze.py
+++ analyze.py
@@ -14,10 +14,10 @@ bod['Name'] = bod.apply(lambda x: re.sub(r'(^\w{2,3}\. ?)', r'', x['Name']),axis
 bod_agg = bod.groupby(by=['Name','YearOfBirth'])['StockCode'].count().reset_index()
 bod_agg[bod_agg['StockCode']>4].sort_values(by='StockCode',ascending=False)
 bod_agg['id'] = bod_agg.index
-
+bod_agg = bod_agg[bod_agg['StockCode']>1]
 #Left join id to intial table
 edge = bod.merge(bod_agg[['Name','YearOfBirth','id']],
-                    how='left', 
+                    how='inner', 
                     left_on=['Name','YearOfBirth'], 
                     right_on=['Name','YearOfBirth'])[['id','StockCode']]
 edge.StockCode.describe()
@@ -26,14 +26,14 @@ def map_data():
     g = Network(height='1500px',width ='100%',bgcolor='#222222',font_color='white')
     for index, row in bod_agg.iterrows():
         if row['StockCode'] >=2:
-            g.add_node(row['id'], label =row['Name'], color='#FF3008')
+            g.add_node(row['id'], label =row['Name']+','+str(row['YearOfBirth']), color='#FF3008')
         else:
             g.add_node(row['id'], label =row['Name'])
     for index, row in edge.iterrows():
         g.add_node(row['StockCode'], color ='#DED007')
         g.add_edge(row['StockCode'],row['id'])
     g.barnes_hut()
-    g.show("test_v2.html")
+    g.show("test_v3.html")
 
 map_data()
 
