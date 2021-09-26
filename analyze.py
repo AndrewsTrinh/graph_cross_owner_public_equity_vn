2f0716c115a60d840eec4daf80d04a5269924d52
Andrews <andrews.trinh@gmail.com>
2021-09-26 10:47:41 +1000

    update csv

--- analyze.py
+++ analyze.py
@@ -3,9 +3,9 @@ import numpy as np
 import re
 import matplotlib.pyplot as plt
 from pyvis.network import Network
-bod = pd.read_csv('bod.csv', index_col =[0])
+bod = pd.read_csv('./python/bod.csv', index_col =[0])
 bod.columns
-company = pd.read_csv('company_list.csv', index_col=[0])
+company = pd.read_csv('./python/company_list.csv', index_col=[0])
 company.head(10)
 bod.drop_duplicates(subset = ['Name', 'YearOfBirth', 'StockCode'],keep = False, inplace=True)
 bod['Name'] = bod.apply(lambda x: re.sub(r'(^\w{2,3}\. ?)', r'', x['Name']),axis=1)
@@ -19,20 +19,22 @@ bod_agg = bod_agg[bod_agg['StockCode']>1]
 edge = bod.merge(bod_agg[['Name','YearOfBirth','id']],
                     how='inner', 
                     left_on=['Name','YearOfBirth'], 
-                    right_on=['Name','YearOfBirth'])[['id','StockCode']]
+                    right_on=['Name','YearOfBirth'])[['id','StockCode','Position']]
 edge.StockCode.describe()
-
+edge
 def map_data():
     g = Network(height='1500px',width ='100%',bgcolor='#222222',font_color='white')
     for index, row in bod_agg.iterrows():
         if row['StockCode'] >=5:
-            g.add_node(row['id'], label =row['Name']+','+str(row['YearOfBirth']), color='purple',size=150)
+            g.add_node(row['id'], label =row['Name']+','+str(row['YearOfBirth']), color='purple',size=75,physics=False)
         else:
-            g.add_node(row['id'], label =row['Name'])
+            g.add_node(row['id'], label =row['Name']+','+str(row['YearOfBirth']),physics=False)
     for index, row in edge.iterrows():
         g.add_node(row['StockCode'], color ='#DED007')
-        g.add_edge(row['StockCode'],row['id'])
+        g.add_edge(row['StockCode'],row['id'], label=row['Position'])
     g.barnes_hut()
+    # g.force_atlas_2based()
+    g.show_buttons(filter_=['physics'])
     g.show("result.html")
 
 map_data()
